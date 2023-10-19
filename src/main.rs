44c020ae84151bbccc076c0abc7f1cba3d90559a
Brett Smith <brettsmith212@gmail.com>
2023-10-19 00:10:50 +0000

    200 status

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -8,10 +9,20 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_client(_stream)
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+
+fn handle_client(mut stream: TcpStream) {
+  let mut buffer = [0; 1024];
+  stream.read(&mut buffer).unwrap();
+  let response = "HTTP/1.1 200 OK\r\n\r\n";
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
 }
