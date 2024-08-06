757f76a4c1fbcbde0d1688690428e2ff0b40b824
brettsmith212 <brettsmith212@gmail.com>
2024-08-05 21:25:02 -0700

    parse

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                stream
-                    .write(b"HTTP/1.1 200 OK\r\n\r\n")
-                    .expect("200 expected\n");
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let request_line: Vec<&str> = http_request[0].split(" ").collect();
+
+    let resp = match request_line[1] {
+        "/" => "200 OK",
+        _ => "404 Not Found",
+    };
+
+    let response = format!("HTTP/1.1 {}\r\n\r\n", resp);
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
